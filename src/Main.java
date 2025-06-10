9f246beb7abf66e1bfdbb4a3e4ae3765125f56d6
HimanshuGit1310 <himanshujhariyazeal@gmail.com>
2025-06-10 15:45:24 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String filePath = "src\\For Granted - Dyalla.wav.crdownload";
+        File file = new File(filePath);
+
+        try(AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file)) {
+
+            Scanner scanner = new Scanner(System.in);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            System.out.println("clip is running");
+
+            String response = "";
+
+            while(!response.equals("Q")) {
+
+                System.out.println("P = Play");
+                System.out.println("S = Stop");
+                System.out.println("R = Reset");
+                System.out.println("Q = Quit");
+                System.out.print("Enter Your Choice : ");
+                response = scanner.next().toUpperCase();
+
+                switch(response) {
+                    case "P" -> clip.start();
+                    case "S" -> clip.stop();
+                    case "R" -> clip.setMicrosecondPosition(0);
+                    case "Q" -> clip.close();
+                    default -> System.out.println("Invalid Choice");
+                }
+
+            }
+
+        }
+
+
+        catch (IOException e) {
+            System.out.println("Could not locate the file");
+        }
+        catch (UnsupportedAudioFileException e) {
+            System.out.println("File does not supported");
+        }
+        catch (LineUnavailableException e) {
+            System.out.println("resource not found");
+        }
+        finally {
+            System.out.println("You have Exited");
         }
     }
 }
+
+
